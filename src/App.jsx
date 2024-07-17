7b3cd9b30f1f68315832ba2c0c5df0d28984951b
Gabriel Quiroga <gabrielnahuel.quiroga@gmail.com>
2024-07-16 23:10:03 -0300

    feature pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,80 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import Card from "./components/card/Card"
-import Container from "./components/container/Container"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/card/Card";
+import Container from "./components/container/Container";
 
 function App() {
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  };
 
-  const [pattern, setPattern] = useState([])
+  const [pattern, setPattern] = useState([1, 0, 3, 2]);
 
   const [cards, setCards] = useState([
     {
       id: 0,
-      flipped: false
+      flipped: false,
     },
     {
       id: 1,
-      flipped: false
+      flipped: false,
     },
     {
       id: 2,
-      flipped: false
+      flipped: false,
     },
     {
       id: 3,
-      flipped: false
+      flipped: false,
     },
-  ])
+  ]);
 
-  const patron = [1, 3, 0, 2]
+  const generateNewPattern = () => {
+    const newPattern = [];
+    for (let i = 0; i < 4; i++) {
+      newPattern.push(getRandomInt(cards.length));
+    }
+    return newPattern;
+  };
 
-  // const cardsData = [
-  //   {
-  //     id:0,
-  //     flipped: false
-  //   },
-  //   {
-  //     id:1,
-  //     flipped: false
-  //   },
-  //   {
-  //     id:2,
-  //     flipped: false
-  //   },
-  //   {
-  //     id:3,
-  //     flipped: false
-  //   },
-
-  // ]
-
-  const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  }
-
-
-
-
-
-  // useEffect(()=> {
-  //   setTimeout(() => {
-  //     setCards((prev)=> prev.map(()=> patron.includes(prev.id) ? {...prev,flipped: !prev.flipped}: prev))
-      
-  //   }, 1200);
-
-  // },[cards])
-
+  const playPattern = (newPattern) => {
+    newPattern.forEach((id, index) => {
+      setTimeout(() => {
+        setCards((prevCards) =>
+          prevCards.map((card) =>
+            card.id === id ? { ...card, flipped: true } : card
+          )
+        );
+        setTimeout(() => {
+          setCards((prevCards) =>
+            prevCards.map((card) =>
+              card.id === id ? { ...card, flipped: false } : card
+            )
+          );
+        }, 500);
+      }, index * 1000);
+    });
+  };
 
   const handleClick = (e) => {
     e.preventDefault();
+    const newPattern = generateNewPattern();
+    setPattern(newPattern);
+    playPattern(newPattern);
+  };
 
-    setCards(prevCards =>
-      prevCards.map(card =>
-        patron.includes(card.id) ?
-          { ...card, flipped: !card.flipped } :
-          card
-      )
-    );
-  }
-
-
-
-
-
-return (
-  <>
-
-    <Container>
-      <button onClick={handleClick}  className=' bg-slate-900 h-14 text-white'>empezar</button>
-      {
-        cards.map((card) => {
-          return <Card key={card.id} id={card.id} flipped={card.flipped} patron={patron} />
-        })
-      }
-    </Container>
-  </>
-)
+  return (
+    <>
+      <Container>
+        <button onClick={handleClick} className=" bg-slate-900 h-14 text-white">
+          empezar
+        </button>
+        {cards.map((card) => {
+          return <Card key={card.id} id={card.id} flipped={card.flipped} />;
+        })}
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
